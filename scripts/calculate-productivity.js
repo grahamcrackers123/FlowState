8d6cb0d5aa52eaf8f52d6e67ccda2ed1c0b0abca
Claire Monghit <lr.dcmonghit@mmdc.mcl.edu.ph>
2025-12-13 19:21:56 +0800

    starting calculate-productivity js

---
+++ scripts/calculate-productivity.js
@@ -0,0 +1,98 @@
+class SiteEntry {
+    constructor(name, minutes, category) {
+        this.name = name;
+        this.minutes = minutes;
+        this.category = category;
+    }
+}
+
+class Store {
+    static getEntries() {
+        let entries;
+        if(localStorage.getItem('entries') === null) {
+            entries = [];
+        } else {
+            entries = JSON.parse(localStorage.getItem('entries'));
+        }
+        return entries;
+    }
+}
+
+class UI {
+    static displayList() {
+        const storedEntries = [
+            {
+                name: 'sample.com',
+                minutes: 23,
+                category: 'Productive'
+            },
+            {
+                name: 'sample-two.com',
+                minutes: 23,
+                category: 'Unproductive'
+            }
+        ];
+
+        const entries = storedEntries;
+
+        entries.forEach((entry) => UI.addEntryToList(entry));
+        
+    }
+
+    static addEntryToList(entry) {
+        const list = document.getElementById('entry-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${entry.name}</td>
+            <td>${entry.minutes}</td>
+            <td>${entry.category}</td>
+            <td><a href="#" class="delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static clearFields() {
+        document.getElementById('site-name').value = '';
+        document.getElementById('min-duration').value = '';
+        document.querySelector('input[name="site-category"]:checked');
+    }
+
+    static deleteEntry(el) {
+        if(el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', UI.displayList);
+
+document.getElementById('website-entry-form').addEventListener('submit', (e) => {
+    // delete after
+    e.preventDefault();
+
+    // get values inputted
+    const name = document.getElementById('site-name').value;
+    const minutes = Number(document.getElementById('min-duration').value);
+    const checkedCategory = document.querySelector('input[name="site-category"]:checked');
+
+    // validation before values get submitted
+    const noRadioButtonsChecked = !document.querySelector('input[name="site-category"]:checked');
+    if(name === '' || Number.isNaN(minutes) || !checkedCategory) {
+        alert('Plase fill in all fields.');
+    } else {
+        const category = checkedCategory.value;
+
+        // instantiate entry
+        const entry = new SiteEntry(name, minutes, category);
+
+        UI.addEntryToList(entry);
+
+        // UI.clearFields();
+        e.target.reset();
+    }
+});
+
+document.getElementById('entry-list').addEventListener('click', (e) => {
+    UI.deleteEntry(e.target)
+});
--- scripts/menu-two.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('menu-button');
-const navPart = document.getElementById('nav');
-
-menu.addEventListener('click', () => {
-    navPart.classList.toggle('active');
-} );
